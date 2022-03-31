1f446e3c0d75490c60290c1d2f900f3335ba1ca2
poeli <po-e@lanl.gov>
2022-03-31 11:25:29 -0600

    update

--- index.py
+++ index.py
@@ -1,9 +1,8 @@
 from cmath import log
 import plotly.express as px
 import dash
-from dash import dcc, html
+from dash import Dash, dcc, html, State, Input, Output, dash_table
 import dash_bootstrap_components as dbc
-from dash.dependencies import Input, Output
 import logging
 import sys
 from logging import Formatter
@@ -20,6 +19,9 @@ filename = "data/SraRunTable_wastewater.csv"
 df = pd.read_pickle(f'{filename}.pkl')
 ddf = df
 
+param_data = {}
+default_color_data = {}
+
 dimensions_dict = {
     'Assay type': 'Assay Type', 
     'Library source': 'LibrarySource', 
@@ -29,17 +31,16 @@ dimensions_dict = {
 }
 dimensions_display = ['Assay Type', 'LibrarySource', 'Platform', 'geo_loc_name_country_continent']
 
-def fig_parallel_categories(df, dimensions, color_col):
+def fig_parallel_categories(df, dimensions, color_data):
     """
     Generate parallel_categories plot
     """
     ddf = df.copy()
     
     # set colors
-    c = list(ddf[color_col].unique())
-    colors = px.colors.qualitative.Dark24 + px.colors.qualitative.Light24
-    c_dict = dict(zip(c, colors[:len(c)]))
-    ddf['COLORS'] = ddf[color_col].map(c_dict)
+    coloring_field = color_data['field']
+    color_map = color_data['color_map']
+    ddf['COLORS'] = ddf[coloring_field].map(color_map)
     
     fig = px.parallel_categories(ddf, dimensions=dimensions, color='COLORS')
     fig.update_layout(
@@ -49,11 +50,11 @@ def fig_parallel_categories(df, dimensions, color_col):
     return fig
 
 
-def fig_spotlen_bases(df, color_col):
+def fig_spotlen_bases(df, coloring_field):
     fig = px.scatter(df, 
                      x="AvgSpotLen", 
                      y="Bases", 
-                     color=color_col, 
+                     color=coloring_field, 
                      log_x=True, 
                      log_y=True,
                      hover_name="Run",
@@ -170,6 +171,114 @@ def dropdown_div(dimensions_dict):
     return input_list
 
 
+def field_color_mapping(df, coloring_field):
+    """
+    Generating color mapping dict
+
+    :param df: dataframe
+    :param coloring_field: string
+    :return dict:
+    """
+    if not coloring_field:
+        coloring_field='Assay Type'
+    
+    # add colors
+    c = list(df[coloring_field].value_counts().sort_values(ascending=False).keys())
+
+    colors = ['#AAAAAA'] + px.colors.qualitative.Dark24 + px.colors.qualitative.Light24
+    
+    if 'N/A' in c:
+        c.remove('N/A')
+        c = ['N/A']+c
+    else:
+        colors.remove('#AAAAAA')
+    
+    c_dict = dict(zip(c, colors[:len(c)]))
+    
+    return {'field': coloring_field, 'color_map': c_dict}
+
+
+def generate_fig_sample_time(dff, color_data):
+    """
+    Generate barplot over time scale for SRA records
+    """
+
+    coloring_field = color_data['field']
+    color_map = color_data['color_map']
+    
+    df = dff.groupby(['week', coloring_field]).count()['Run'].reset_index()
+    df.week = df.week.astype(str)
+    
+    logging.info(df)
+
+    fig = px.bar(
+        df, 
+        x='week', 
+        y='Run',
+        color=coloring_field,
+        color_discrete_map=color_map,
+        template='simple_white',
+        log_y=True,
+        custom_data=[coloring_field],
+        labels={
+             "week": "Week of collection",
+             "Run": "Num of runs"
+         },
+    )
+    
+    fig.update_traces(
+         hovertemplate="Collection date: %{x}<br>" +
+                       "Number of runs: %{y}<br><br>"
+    )
+    
+    # Add range slider
+    fig.update_layout(
+        xaxis=dict(
+            rangeselector=dict(
+                buttons=list([
+                    dict(count=1,
+                         label="1m",
+                         step="month",
+                         stepmode="backward"),
+                    dict(count=6,
+                         label="6m",
+                         step="month",
+                         stepmode="backward"),
+                    dict(count=1,
+                         label="YTD",
+                         step="year",
+                         stepmode="todate"),
+                    dict(count=1,
+                         label="1y",
+                         step="year",
+                         stepmode="backward"),
+                    dict(step="all")
+                ])
+            ),
+            rangeslider=dict(
+                visible=True
+            ),
+            type="date"
+        )
+    )
+
+    fig.update_layout(
+        font_size=11,
+        title_font_family="Helvetica, Arial, sans-serif",
+        height=500,
+        legend=dict(
+          orientation="h",
+          xanchor="left",
+          yanchor="bottom",
+          x=0,
+          y=-0.7,
+        ),
+        # plot_bgcolor='rgb(233,233,233, 0.1)',
+    )
+
+    return fig
+
+
 # layout
 input_list = dropdown_div(dimensions_dict)
 
@@ -185,6 +294,8 @@ input_list.append(col)
 layout_dcc = html.Div(
     children=[
         dcc.Store(id='aggregate_data'),
+        dcc.Store(id='center_data'),
+        dcc.Store(id='color_mapping'),
         html.H2(
             children='Wastewater metagenome',
             style={'color': '#333333'}
@@ -260,7 +371,7 @@ layout_dcc = html.Div(
 )
 
 ########### Initiate the app
-app = dash.Dash(__name__, external_stylesheets=[dbc.themes.PULSE])
+app = Dash(__name__, external_stylesheets=[dbc.themes.PULSE])
 app.title = "SRA-Wastewater"
 server = app.server
 
@@ -268,23 +379,25 @@ app.layout = layout_dcc
 
 @app.callback(
     Output('aggregate_data', 'data'),
+    Output('center_data', 'data'),
     Input('Assay type', 'value'),
     Input('Library source', 'value'),
     Input('Platform', 'value'),
     Input('Continent', 'value'),
     Input('Country', 'value'),
-    Input('colored_column', 'value'),
+    State('colored_column', 'value'),
 )
-def update_df(assay_type, library_source, platform, continent, country, colored_column):
+def update_agg_data(assay_type, library_source, platform, continent, country, coloring_field):
     global ddf
+    # global param_data
+    global default_color_data
 
-    data = dict(
+    param_data = dict(
         assay_type = assay_type,
         library_source = library_source, 
         platform = platform, 
         continent = continent,
         country = country, 
-        colored_column = colored_column,
     )
 
     query_list = []
@@ -315,7 +428,22 @@ def update_df(assay_type, library_source, platform, continent, country, colored_
         logging.debug(query_text)
         ddf = df.query(query_text)
 
-    return data
+    # store all avail center
+    center_data = ddf['Center Name'].unique()
+
+    default_color_data = field_color_mapping(ddf, coloring_field)
+
+    return param_data, center_data
+
+
+# coloring field controllers -> color mapping
+@app.callback(Output('color_mapping', 'data'),
+              Input('colored_column', 'value'),
+             )
+def update_color_map(coloring_field):
+    global ddf
+
+    return field_color_mapping(ddf, coloring_field)
 
 
 @app.callback(
@@ -323,25 +451,31 @@ def update_df(assay_type, library_source, platform, continent, country, colored_
     Output('stats_output', 'children'),
     Output('center_name_dropdown', 'options'),
     Input('aggregate_data', 'data'),
+    Input('center_data', 'data'),
+    Input('color_mapping', 'data'),
 )
-def update_sankey_graph(data):
+def update_sankey_graph(data, center_data, color_data):
     global ddf
+    global default_color_data
+    
+    if not color_data: color_data = default_color_data
 
-    fig = fig_parallel_categories(ddf, dimensions_display, data['colored_column'])
+    fig = fig_parallel_categories(ddf, dimensions_display, color_data)
     
     # generate options for center_name_dropdown
-    options = [{'label': center_name, 'value': center_name} for center_name in ddf['Center Name'].unique()]
+    options = [{'label': center_name, 'value': center_name} for center_name in center_data]
 
     return fig, get_stats(ddf), options
+
 
 @app.callback(
     Output('sra_scatter', 'figure'),
     Output('sra_geo', 'figure'),
     Output('bases_stats_output', 'children'),
     Input('center_name_dropdown', 'value'),
-    Input('aggregate_data', 'data'),
+    Input('color_mapping', 'data'),
 )
-def update_graph(selected_center, data):
+def update_graph(selected_center, color_data):
     global ddf
     ddf_center = ddf
 
@@ -351,8 +485,9 @@ def update_graph(selected_center, data):
         ddf_center = ddf[idx]
 
     # cross-filtering with sequencing centers
-    fig_sc = fig_spotlen_bases(ddf_center, color_col='Center Name')    
-
+    # fig_sc = fig_spotlen_bases(ddf_center, 'Center Name')
+    fig_sc = generate_fig_sample_time(ddf_center, color_data)
+    
     # generate geo plots
     fig_geo = fig_geo_stats(ddf_center)
 
@@ -372,4 +507,4 @@ def func(n_clicks):
         return dict(content=content, filename="sra_wastewater.csv")
 
 if __name__ == '__main__':
-    app.run_server(debug=False)
+    app.run_server(debug=True)
